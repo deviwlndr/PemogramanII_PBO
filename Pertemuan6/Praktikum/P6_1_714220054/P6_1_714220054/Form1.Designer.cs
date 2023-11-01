24c782aade60632bed4082c0c11b4053d66cec54
deviwlndrr <wdevi443@gmail.com>
2023-11-01 16:34:36 +0700

    push p6

---
+++ Pertemuan6/Praktikum/P6_1_714220054/P6_1_714220054/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace P6_1_714220054
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.vbButton = new System.Windows.Forms.Button();
+            this.csButton = new System.Windows.Forms.Button();
+            this.closeButtun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Teal;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(790, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Which do you think is better, Visual Basic or C#?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // vbButton
+            // 
+            this.vbButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.vbButton.Font = new System.Drawing.Font("MS Reference Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vbButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.vbButton.Location = new System.Drawing.Point(114, 134);
+            this.vbButton.Name = "vbButton";
+            this.vbButton.Size = new System.Drawing.Size(156, 39);
+            this.vbButton.TabIndex = 1;
+            this.vbButton.Text = "VIsual Basic";
+            this.vbButton.UseVisualStyleBackColor = false;
+            this.vbButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.vbButton_MouseMove);
+            // 
+            // csButton
+            // 
+            this.csButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.csButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.csButton.ForeColor = System.Drawing.Color.Red;
+            this.csButton.Location = new System.Drawing.Point(488, 134);
+            this.csButton.Name = "csButton";
+            this.csButton.Size = new System.Drawing.Size(98, 39);
+            this.csButton.TabIndex = 2;
+            this.csButton.Text = "C#";
+            this.csButton.UseVisualStyleBackColor = false;
+            this.csButton.Click += new System.EventHandler(this.csButton_Click);
+            // 
+            // closeButtun
+            // 
+            this.closeButtun.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.closeButtun.Font = new System.Drawing.Font("Trebuchet MS", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeButtun.ForeColor = System.Drawing.Color.Black;
+            this.closeButtun.Location = new System.Drawing.Point(34, 386);
+            this.closeButtun.Name = "closeButtun";
+            this.closeButtun.Size = new System.Drawing.Size(97, 36);
+            this.closeButtun.TabIndex = 3;
+            this.closeButtun.Text = "Close";
+            this.closeButtun.UseVisualStyleBackColor = false;
+            this.closeButtun.Click += new System.EventHandler(this.closeButtun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.closeButtun);
+            this.Controls.Add(this.csButton);
+            this.Controls.Add(this.vbButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button vbButton;
+        private System.Windows.Forms.Button csButton;
+        private System.Windows.Forms.Button closeButtun;
+    }
+}
+
---
+++ Pertemuan6/Praktikum/P6_3_714220054/P6_3_714220054/P6_3_714220054/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace P6_3_714220054
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtHuruf_Leave(object sender, EventArgs e)
+        {
+            if (txtHuruf.Text == "")
+            {
+                epWarning.SetError(txtHuruf, "textbox Huruf tidak boleh kosong!");
+            }
+            else
+            {
+
+
+                if ((txtHuruf.Text).All(Char.IsLetter))
+                {
+                    epWarning.SetError(txtHuruf, "");
+                    epWrong.SetError(txtHuruf, "");
+                    epCorrect.SetError(txtHuruf, "Betul Betul Betul!");
+                }
+                else
+                {
+                    epWrong.SetError(txtHuruf, "Inputan hanya boleh huruf!");
+                    epWarning.SetError(txtHuruf, "");
+                    epCorrect.SetError(txtHuruf, "");
+                }
+            }
+
+        }
+
+        private void txtAngka_Leave(object sender, EventArgs e)
+        {
+            if (txtAngka.Text == "")
+            {
+                epWarning.SetError(txtAngka, "textbox Huruf tidak boleh kosong!");
+            }
+            else
+            {
+                if ((txtAngka.Text).All(Char.IsNumber))
+                {
+                    epCorrect.SetError(txtAngka, "Betul!");
+                    epWarning.SetError(txtAngka, "");
+                    epWrong.SetError(txtAngka, "");
+                }
+                else
+                {
+                    epCorrect.SetError(txtAngka, "");
+                    epWarning.SetError(txtAngka, "");
+                    epWrong.SetError(txtAngka, "Inputan hanya boleh angka!");
+                }
+            }
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEmail_Leave(object sender, EventArgs e)
+        {
+            if (txtEmail.Text == "")
+            {
+                epWarning.SetError(txtEmail, "textbox Huruf tidak boleh kosong!");
+            }
+            else
+            {
+                if (Regex.IsMatch(txtEmail.Text, @"^^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+                {
+                    epWarning.SetError(txtEmail, "");
+                    epWrong.SetError(txtEmail, "");
+                    epCorrect.SetError(txtEmail, "Betul!");
+                }
+                else
+                {
+                    epWrong.SetError(txtEmail, "Format email salah!\nContoh: a@b.c");
+                    epWarning.SetError(txtEmail, "");
+                    epCorrect.SetError(txtEmail, "");
+                }
+            }
+        }
+
+        private void txtAngka1_Leave(object sender, EventArgs e)
+        {
+            if (txtAngka1.Text == "")
+            {
+                epWarning.SetError(txtAngka1, "textbox Huruf tidak boleh kosong!");
+            }
+            else
+            {
+                if (int.TryParse(txtAngka1.Text, out int angka1) && int.TryParse(txtAngka2.Text, out int angka2))
+                {
+                    if (angka1 > angka2)
+                    {
+                        epCorrect.SetError(txtAngka1, "Betul!");
+                        epWarning.SetError(txtAngka1, "");
+                        epWrong.SetError(txtAngka1, "");
+                    }
+                    else
+                    {
+                        epCorrect.SetError(txtAngka1, "");
+                        epWarning.SetError(txtAngka1, "");
+                        epWrong.SetError(txtAngka1, "Angka1 harus lebih besar dari Angka2.");
+                    }
+                }
+                else
+                {
+                    epCorrect.SetError(txtAngka1, "");
+                    epWarning.SetError(txtAngka1, "");
+                    epWrong.SetError(txtAngka1, "Inputan hanya boleh angka!");
+                }
+            }
+        }
+
+        private void txtAngka2_Leave(object sender, EventArgs e)
+        {
+            if (txtAngka2.Text == "")
+            {
+                epWarning.SetError(txtAngka2, "textbox Huruf tidak boleh kosong!");
+            }
+            else
+            {
+                if (int.TryParse(txtAngka1.Text, out int angka1) && int.TryParse(txtAngka2.Text, out int angka2))
+                {
+                    if (angka1 > angka2)
+                    {
+                        epCorrect.SetError(txtAngka2, "Betul!");
+                        epWarning.SetError(txtAngka2, "");
+                        epWrong.SetError(txtAngka2, "");
+                    }
+                    else
+                    {
+                        epCorrect.SetError(txtAngka2, "");
+                        epWarning.SetError(txtAngka2, "");
+                        epWrong.SetError(txtAngka2, "Angka2 harus lebih kecil dari Angka1.");
+                    }
+                }
+                else
+                {
+                    epCorrect.SetError(txtAngka2, "");
+                    epWarning.SetError(txtAngka2, "");
+                    epWrong.SetError(txtAngka2, "Inputan hanya boleh angka!");
+                }
+            }
+        }
+    }
+}
